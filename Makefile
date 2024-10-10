657fb91bc8f724c6b49fda5e52b61b8c5c1c8c86
Enrique Marín <enriquemarin_sierra@hotmail.com>
2024-10-10 20:26:58 +0200

    add workflows for github actions

--- Makefile
+++ Makefile
@@ -4,8 +4,29 @@ run:
 build:
 	go build -o bin/todoapp .
 
+build-prod:
+	go build -ldflags="-s -w" -o ./bin/todoapp -tags prod
+
 clean:
 	rm -rf bin/
 
-release:
-	fyne package --release -exe todoapp
+# Only for releases on Github Actions
+generate-textfiles:
+	sh scripts/generate_license-readme.sh
+
+package-linux:
+	sh scripts/update_version.sh
+	fyne package -os linux --release -exe ftodo --tags prod
+	sh scripts/restore_version.sh
+
+package-windows:
+	sh scripts/update_version.sh
+	fyne package -os windows --release -exe bin/ftodo.exe --tags prod --appID com.emarifer.ftodo
+	sh scripts/restore_version.sh
+
+package-darwin:
+	mkdir bin
+	sh scripts/update_version.sh
+	fyne package -os darwin --release -exe ftodo.app --tags prod
+	mv ftodo.app bin/
+	sh scripts/restore_version.sh
---
+++ scripts/generate_license-readme.sh
@@ -0,0 +1,40 @@
+cat > LICENSE.txt << EOF
+The MIT License (MIT)
+
+Copyright © 2024 Enrique Marín
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+EOF
+cat > README.txt << EOF
+INSTALLING fToDo App:
+=====================
+
+Since the supplied executable contains everything needed to run on Windows,
+it can be considered portable software.
+This means that you can run it from any location by simply double-clicking on it.
+
+However, if you want to use it as an "installed" application
+you can place the executable in the directory "C:\Program Files".
+You will then need to add it to the system PATH as explained here:
+https://www.eukhost.com/kb/how-to-add-to-the-path-on-windows-10-and-windows-11/
+
+================================================================================
+
+Enjoy it!!
+EOF
